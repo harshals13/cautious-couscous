026be574e156e7a7a7a2ba32064d9bb9f3eb1a08
harshals13 <harshasuryavamshi@gmail.com>
2019-10-07 00:28:03 +0530

    changes

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {
     port: process.env.PORT || 3000,
     zomatoUrl: 'https://developers.zomato.com/api/v2.1/',
-    userKey: 'a6fa34ef42e914dc2df19d474256f232'
+    userKey: ''
 };
 
 module.exports = config;
